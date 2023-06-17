d64b7d4cb4d3b2e153dd834bb94f96d557dbfc06
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-06-17 15:57:34 +0200

    Fix issue where JVM would be shut down prematurely

--- python/src/autobi/core/jvm_view.py
+++ python/src/autobi/core/jvm_view.py
@@ -1,3 +1,6 @@
+import subprocess
+import gc
+
 from collections import defaultdict
 from dataclasses import dataclass
 
@@ -6,14 +9,20 @@ from py4j.java_gateway import JavaGateway, JVMView
 from typing import Optional
 
 from autobi._jar import JARPATH
+from .not_instantiable import NotInstantiable
+
+
+class JVMNotRunning(Exception):
+    pass
 
 
 class _JVM:
     """
-    The underlying JVM management
+    The underlying JVM management Singleton
 
     This class performs the management of the JVM and its views behind the scenes,
     allowing for py4j to be interacted with with pythonic __enter__ and __exit__ methods
+    It contains a *lot* of ugly C-style resource management. Sadly, this is required
 
     This class is private, and not designed to be interacted with by users.
     """
@@ -22,41 +31,80 @@ class _JVM:
     class AutobiJVM:
         _jvm: JVMView
 
+    # It seems like this can be made cleaner with one dataclass that contains all three,
+    # saving an optional instance of that dataclass.
+    # However, because of the reference counting, this creates only more chaos
     gateway: Optional[JavaGateway] = None
     views: Optional[defaultdict[str, AutobiJVM]] = None
+    ref_count: Optional[defaultdict[str, int]] = None
+
+    @classmethod
+    def is_in_valid_state(cls) -> bool:
+        if not (cls.gateway is None) == (cls.views is None) == (cls.ref_count is None):
+            return False
+
+        if cls.views is not None and cls.ref_count is not None:
+            if not set(cls.views.keys()) == set(cls.ref_count.keys()):
+                print(cls.ref_count.keys(), cls.views.keys())
+                return False
+
+        return True
+
+    @classmethod
+    def is_running(cls) -> bool:
+        assert cls.is_in_valid_state()
+        return cls.views is not None
 
     @classmethod
     def get_view(cls, name: str = "default") -> AutobiJVM:
-        if cls.gateway is None or cls.views is None:
-            cls.gateway = JavaGateway.launch_gateway(classpath=str(JARPATH))
+        if not cls.is_running():
+            cls.gateway = JavaGateway.launch_gateway(
+                classpath=str(JARPATH),
+                redirect_stdout=subprocess.STDOUT,
+                redirect_stderr=subprocess.PIPE,
+            )
             cls.views = defaultdict(lambda: cls.AutobiJVM(cls.gateway.new_jvm_view()))
+            cls.ref_count = defaultdict(lambda: 0)
 
-        return cls.views[name]
+        cls.ref_count[name] += 1
+        result = cls.views[name]
+        assert cls.is_in_valid_state()
+        return result
 
     @classmethod
     def delete_view(cls, name: str = "default") -> None:
-        if cls.gateway is None or cls.views is None:
-            return
+        if not cls.is_running():
+            raise JVMNotRunning("Cannot delete view: JVM is not running")
 
-        del cls.views[name]
+        if not ((name in cls.ref_count.keys()) and (name in cls.views.keys())):
+            raise ValueError(f"Cannot find view: {name}")
 
-        if not cls.views:
-            cls.gateway.shutdown()
-            cls.gateway = None
-            cls.views = None
+        cls.ref_count[name] -= 1
+        if cls.ref_count[name] <= 0:
+            del cls.views[name]
+            del cls.ref_count[name]
+
+        assert cls.is_in_valid_state()
+
+        if not cls.views.keys():
+            cls.shutdown()
 
     @classmethod
     def shutdown(cls) -> None:
+        if cls.views is not None:
+            cls.views.clear()
+            del cls.views
+        if cls.ref_count is not None:
+            cls.ref_count.clear()
+            del cls.ref_count
         if cls.gateway is not None:
             cls.gateway.shutdown()
             del cls.gateway
-        if cls.views is not None:
-            cls.views.clear()
+            # gc.collect is recommended to be called by the py4j docs after shutdown.
+            gc.collect()
         cls.gateway = None
         cls.views = None
-
-    def __del__(self):
-        self.__class__.shutdown()
+        cls.ref_count = None
 
 
 class AutobiJVMHandler:
@@ -77,20 +125,31 @@ class AutobiJVMHandler:
         return self.handler
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
-        _JVM.delete_view(self.name)
+        try:
+            _JVM.delete_view(self.name)
+        except JVMNotRunning:
+            # This means _shutdown was called somewhere halfway
+            pass
 
 
 def _shutdown():
+    """The public shutdown function of the JVM.
+    It is kept seperate from _JVM to allow it to be registered with atexit
+    """
     _JVM.shutdown()
 
 
-# Prevent users from instantiating an instance of this class themselves.
-# However, because of the .pyi file, AutobiJVM will still be recognised as _JVM.AutobiJVM
-#    by the python interpreter and any type checker
-AutobiJVM = None
+class AutobiJVM:
+    _jvm: JVMView
+
+    def __new__(cls):
+        raise NotInstantiable(
+            "AutobiJVM object can only be created by the AutobiJVMHandler"
+        )
+
 
 # Disallow usage of _JVM
-__all__ = ("AutobiJVMHandler", "AutobiJVM")
+__all__ = ("AutobiJVMHandler", "AutobiJVM", "_shutdown")
 
 # Ensure that the JVM will always be shut down when the application is closed
 import atexit
--- python/tests/test_jvm.py
+++ python/tests/test_jvm.py
@@ -1,7 +1,7 @@
 from py4j.java_gateway import JavaGateway, JVMView
 import pytest
 
-from autobi.core import AutobiJVM, AutobiJVMHandler
+from autobi.core import AutobiJVM, AutobiJVMHandler, NotInstantiable
 
 
 class TestJVMTyping:
@@ -10,57 +10,82 @@ class TestJVMTyping:
             pass
 
     def test_instance_not_instantiatable(self):
-        with pytest.raises(TypeError):
+        with pytest.raises(NotInstantiable):
             AutobiJVM()
 
 
 class TestJVMManagment:
     def test_name_management_same(self):
-        with AutobiJVMHandler("test") as jvm1:
-            with AutobiJVMHandler("test") as jvm2:
-                assert jvm1 is jvm2
-
-    def test_name_management_different(self):
-        with AutobiJVMHandler("test1") as jvm1:
-            with AutobiJVMHandler("test2") as jvm2:
-                assert jvm1 is not jvm2
-
-    def test_cleanup(self):
-        # Code smell: We are doing soem reaching beneath the scenes to test this.
+        # Code smell: We are doing some reaching beneath the scenes to test this.
         from autobi.core.jvm_view import _JVM
 
+        assert _JVM.is_in_valid_state()
+        with AutobiJVMHandler("test") as jvm1:
+            assert _JVM.is_in_valid_state()
+            with AutobiJVMHandler("test") as jvm2:
+                assert jvm1 is jvm2
+                assert _JVM.is_in_valid_state()
+            assert _JVM.views["test"]
+            assert _JVM.is_in_valid_state()
+
+    def test_name_management_different(self):
+        # Code smell: We are doing some reaching beneath the scenes to test this.
+        from autobi.core.jvm_view import _JVM
+
+        assert _JVM.is_in_valid_state()
+        with AutobiJVMHandler("test1") as jvm1:
+            assert _JVM.is_in_valid_state()
+            with AutobiJVMHandler("test2") as jvm2:
+                assert _JVM.is_in_valid_state()
+                assert jvm1 is not jvm2
+            assert _JVM.is_in_valid_state()
+        assert _JVM.is_in_valid_state()
+
+    def test_cleanup(self):
+        # Code smell: We are doing some reaching beneath the scenes to test this.
+        from autobi.core.jvm_view import _JVM
+
+        assert _JVM.is_in_valid_state()
         assert _JVM.views == None
         assert _JVM.gateway == None
 
         with AutobiJVMHandler("test1") as jvm1:
+            assert _JVM.is_in_valid_state()
             assert "test1" in _JVM.views
             assert not "test2" in _JVM.views
             assert isinstance(_JVM.gateway, JavaGateway)
             assert isinstance(jvm1._jvm, JVMView)
             with AutobiJVMHandler("test2") as jvm2:
+                assert _JVM.is_in_valid_state()
                 assert "test1" in _JVM.views
                 assert "test2" in _JVM.views
                 assert isinstance(_JVM.gateway, JavaGateway)
                 assert isinstance(jvm1._jvm, JVMView)
                 assert isinstance(jvm2._jvm, JVMView)
+            assert _JVM.is_in_valid_state()
             assert "test1" in _JVM.views
             assert not "test2" in _JVM.views
             assert isinstance(_JVM.gateway, JavaGateway)
             assert isinstance(jvm1._jvm, JVMView)
 
+        assert _JVM.is_in_valid_state()
         assert _JVM.views == None
         assert _JVM.gateway == None
 
     def test_hard_shutdown(self):
         # The JVMView items will linger, and re-start the JavaGateway if used.
         # This simply asserts that a shutdown removes all references from this singleton
-        # As far as I can find, there is not function to assert the gateway was actually shut down
+        # As far as I can find, there is no function to assert the gateway was actually shut down
 
         from autobi.core.jvm_view import _shutdown
         from autobi.core.jvm_view import _JVM
 
+        assert _JVM.is_in_valid_state()
         with AutobiJVMHandler("test1") as jvm1:
+            assert _JVM.is_in_valid_state()
             assert "test1" in _JVM.views
             _shutdown()
+            assert _JVM.is_in_valid_state()
             assert _JVM.views == None
             assert _JVM.gateway == None
+        assert _JVM.is_in_valid_state()
